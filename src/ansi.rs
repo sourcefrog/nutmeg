7a08e3320fcc6909a7b7d1a4e67841222f07bcbc
Martin Pool <mbp@sourcefrog.net>
2022-02-19 12:01:11 -0800

    Use `CSI?7l` for wrap
    Seems to work on Windows, at least

--- src/ansi.rs
+++ src/ansi.rs
@@ -5,8 +5,9 @@
 //  ansi::MoveToStartOfLine, ansi::DisableLineWrap, rendered, ansi::ClearToEndOfLine)?;
 pub(crate) const MOVE_TO_START_OF_LINE: &str = "\x1b[1G";
 
-pub(crate) const DISABLE_LINE_WRAP: &str = "\x1b[=7l";
-pub(crate) const ENABLE_LINE_WRAP: &str = "\x1b[=7h";
+// https://vt100.net/docs/vt510-rm/DECAWM
+pub(crate) const DISABLE_LINE_WRAP: &str = "\x1b[?7l";
+pub(crate) const ENABLE_LINE_WRAP: &str = "\x1b[?7h";
 
 pub(crate) const CLEAR_TO_END_OF_LINE: &str = "\x1b[0K";
 pub(crate) const CLEAR_CURRENT_LINE: &str = "\x1b[2K";
