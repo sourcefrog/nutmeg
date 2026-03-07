75e533c702b186b4eb6c1a9ae95cd488463017f8
Martin Pool <mbp@sourcefrog.net>
2026-03-07 07:17:09 -0800

    Use `terminal_size_of` rather than an unsafe API.

--- src/width.rs
+++ src/width.rs
@@ -1,26 +1,28 @@
-// Copyright 2022-2023 Martin Pool
+// Copyright 2022-2026 Martin Pool
 
 //! Measure terminal width.
 
-use terminal_size::Width;
+use std::{
+    io::{stderr, stdout},
+    os::fd::AsFd,
+};
+
+use terminal_size::{terminal_size_of, Width};
+
 #[cfg(unix)]
 pub(crate) fn stdout_width() -> Option<usize> {
-    unsafe {
-        terminal_size::terminal_size_using_fd(1).map(|(Width(w), _)| w as usize)
-    }
+    terminal_size_of(stdout().as_fd()).map(|(Width(w), _)| w as usize)
 }
 
 #[cfg(windows)]
 pub(crate) fn stdout_width() -> Option<usize> {
-    // TODO: We could get the handle for stderr to make this more precise...
+    // TODO: We could get the handle for stdout to make this more precise...
     terminal_size::terminal_size().map(|(Width(w), _)| w as usize)
 }
 
 #[cfg(unix)]
 pub(crate) fn stderr_width() -> Option<usize> {
-    unsafe {
-        terminal_size::terminal_size_using_fd(2).map(|(Width(w), _)| w as usize)
-    }
+    terminal_size_of(stderr().as_fd()).map(|(Width(w), _)| w as usize)
 }
 
 #[cfg(windows)]
